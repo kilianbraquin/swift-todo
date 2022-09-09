ead4b4fe0aba82076a47010384374e796a72012c
Kilian Braquin <kbraquin@indiebaie.com>
2022-09-09 02:48:17 +0200

    comment framer motion usage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
 import SwiftTodoIcon from "@/icons/swift-todo.svg";
-import { useScroll } from "framer-motion";
-import { FC, useEffect, useState } from "react";
+// import { useScroll } from "framer-motion";
+import { FC, useState } from "react";
 
 export const Header: FC = () => {
-  const { scrollY } = useScroll();
+  // const { scrollY } = useScroll();
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  useEffect(() => {
-    setScrollPosition(scrollY.get());
-    return scrollY.onChange((latest) => {
-      console.log("called");
-      setScrollPosition(latest);
-    });
-  }, []);
+  // useEffect(() => {
+  //   setScrollPosition(scrollY.get());
+  //   return scrollY.onChange((latest) => {
+  //     console.log("called");
+  //     setScrollPosition(latest);
+  //   });
+  // }, []);
 
   return (
     <div className="h-16 sticky border-b border-neutral-500 top-0 bg-white flex justify-center items-center">
