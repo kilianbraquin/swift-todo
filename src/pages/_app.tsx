14c1038896a816ff14f1b370ef4fdbddadf9c874
Kilian Braquin <kbraquin@indiebaie.com>
2022-09-09 04:02:36 +0200

    fathom with custom domain

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,23 +8,17 @@ function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
     Fathom.load("HCAHLFVJ", {
-      includedDomains: ["taupe-basbousa-1335e2.netlify.app"],
+      includedDomains: ["swift-todo.pages.dev"],
+      url: "https://alert-energetic.indiebaie.com/script.js",
     });
 
     function onRouteChangeComplete() {
       Fathom.trackPageview();
     }
 
-    // Record a pageview when route changes
     router.events.on("routeChangeComplete", onRouteChangeComplete);
 
-    // Unassign event listener
     return () => {
       router.events.off("routeChangeComplete", onRouteChangeComplete);
     };
