69b3935ffa582aba4c2a835c35831c2f1ac8e6a7
kbraquin <kbraquin@indiebaie.com>
2025-01-12 00:56:37 +0100

    feat: Handle releases with GitHub Actions

--- .release-it.ts
+++ .release-it.ts
@@ -7,6 +7,14 @@ export default {
     requireCommits: true,
     tagName: "v${version}",
   },
+  github: {
+    release: true,
+    // @ts-expect-error - releaseNotes doesn't expect arguments
+    releaseNotes(context) {
+      // Remove the first, redundant line with version and date.
+      return context.changelog.split("\n").slice(1).join("\n");
+    },
+  },
   plugins: {
     "@release-it/conventional-changelog": {
       preset: {
